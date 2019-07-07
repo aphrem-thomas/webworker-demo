e7af3fe1201563ab453352216c4f30ea1c6bd63e
aphremthomas <aphremthomas@gmail.com>
2019-07-07 10:39:21 +0530

    webworkers working

--- worker.js
+++ worker.js
@@ -1,6 +1,9 @@
-this.onMessage = function(e){
+this.onmessage = function(e){
     if(e.data.p !== undefined){
+        var p = e.data.p;
+        var n = e.data.n;
+        var r = e.data.r;
         console.log("received ", e.data);
-        // this.postMessage({})
+        this.postMessage({amount:p*(1+(r/100))^n})
     }
 }
